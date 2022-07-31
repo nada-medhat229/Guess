0df533489385685811b86517bedab760364f5559
nada-medhat229 <nda96642@gmail.com>
2022-07-31 13:30:37 +0200

    update

--- js/main.js
+++ js/main.js
@@ -92,5 +92,5 @@ function ingame(e){
 reset.addEventListener("click",randomWord)
 typing.addEventListener("input",ingame)
 inputs.addEventListener("click",()=> typing.focus())
-document.addEventListener("keyCode",()=> typing.focus(),true)
+document.addEventListener("keypress",()=> typing.focus(),true)
 
